bf31eef26c3d220425952f83d6ff9c52258ac62b
itthinx <itthinx@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2013-09-14 10:14:09 +0000

    checking in version 1.0.0
    git-svn-id: https://plugins.svn.wordpress.org/woocommerce-coupon-shortcodes/trunk@772179 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ lib/views/class-woocommerce-coupon-shortcodes-views.php
@@ -0,0 +1,421 @@
+<?php
+/**
+ * class-woocommerce-coupon-shortcodes-views.php
+ *
+ * Copyright (c) 2013 "kento" Karim Rahimpur www.itthinx.com
+ *
+ * This code is released under the GNU General Public License.
+ * See COPYRIGHT.txt and LICENSE.txt.
+ *
+ * This code is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * This header and all notices must be kept intact.
+ * 
+ * @author Karim Rahimpur
+ * @package woocommerce-coupon-shortcodes
+ * @since woocommerce-coupon-shortcodes 1.0.0
+ */
+
+/**
+ * Shortcodes.
+ */
+class WooCommerce_Coupon_Shortcodes_Views {
+
+	/**
+	 * Adds shortcodes.
+	 */
+	public static function init() {
+		add_shortcode( 'coupon_is_valid', array( __CLASS__, 'coupon_is_valid' ) );
+		add_shortcode( 'coupon_is_not_valid', array( __CLASS__, 'coupon_is_not_valid' ) );
+		add_shortcode( 'coupon_code', array( __CLASS__, 'coupon_code' ) );
+		add_shortcode( 'coupon_description', array( __CLASS__, 'coupon_description' ) );
+		add_shortcode( 'coupon_discount', array( __CLASS__, 'coupon_discount' ) );
+	}
+
+	/**
+	 * Evaluate common validity based on op and coupon codes.
+	 * 
+	 * @param array $atts
+	 * @return boolean
+	 */
+	private static function _is_valid( $atts ) {
+
+		global $woocommerce_coupon_shortcodes_codes;
+
+		$options = shortcode_atts(
+			array(
+				'coupon' => null,
+				'code'   => null,
+				'op'     => 'and'
+			),
+			$atts
+		);
+
+		$code = null;
+		if ( !empty( $options['code'] ) ) {
+			$code = $options['code'];
+		} else if ( !empty( $options['coupon'] ) ) {
+			$code = $options['coupon'];
+		}
+		if ( $code === null ) {
+			return '';
+		}
+
+		$codes = array_map( 'trim', explode( ',', $code ) );
+		$woocommerce_coupon_shortcodes_codes = $codes;
+
+		$validities = array();
+		foreach ( $codes as $code ) {
+			$coupon = new WC_Coupon( $code );
+			if ( $coupon->id ) {
+				$validities[] = $coupon->is_valid();
+			}
+		}
+
+		switch( strtolower( $options['op'] ) ) {
+			case 'and' :
+				$valid = self::conj( $validities );
+				break;
+			default :
+				$valid = self::disj( $validities );
+		}
+
+		return $valid;
+	}
+
+	/**
+	 * Boolean AND on array elements.
+	 * 
+	 * @param array $a
+	 * @return boolean true if all elements are true and there is at least one in the array, false otherwise
+	 */
+	public static function conj( $a ) {
+		$r = false;
+		if ( is_array( $a ) ) {
+			$c = count( $a );
+			if ( $c > 0 ) {
+				$r = true;
+				$i = 0;
+				while( $r && ( $i < $c ) ) {
+					$r = $r && $a[$i];
+					$i++;
+				}
+			}
+		}
+		return $r;
+	}
+
+	/**
+	 * Boolean OR on array elements.
+	 * 
+	 * @param array $a
+	 * @return boolean true if at least one true element is in the array, false otherwise
+	 */
+	public static function disj( $a ) {
+		$r = false;
+		if ( is_array( $a ) ) {
+			$c = count( $a );
+			if ( $c > 0 ) {
+				$r = false;
+				$i = 0;
+				while( !$r && ( $i < $c ) ) {
+					$r = $r || $a[$i];
+					$i++;
+				}
+			}
+		}
+		return $r;
+	}
+
+	/**
+	 * Conditionally render content based on coupon validity.
+	 * 
+	 * Takes a comma-separated list of coupon codes as coupon or code attribute.
+	 * 
+	 * The op attribute determines whether all codes must be valid (and) or
+	 * any code can be valid (or) for the content to be rendered.
+	 * 
+	 * @param array $atts attributes
+	 * @param string $content content to render
+	 * @return string
+	 */
+	public static function coupon_is_valid( $atts, $content = null ) {
+		$output = '';
+		if ( !empty( $content ) ) {
+			$valid = self::_is_valid( $atts );
+			if ( $valid ) {
+				remove_shortcode( 'coupon_is_valid' );
+				$content = do_shortcode( $content );
+				add_shortcode( 'coupon_is_valid', array( __CLASS__, 'coupon_is_valid' ) );
+				$output = $content;
+			}
+		}
+		return $output;
+	}
+
+	/**
+	 * Conditionally render content based on coupon non-validity.
+	 *
+	 * Takes a comma-separated list of coupon codes as coupon or code attribute.
+	 *
+	 * The op attribute determines whether all codes must be valid (and) or
+	 * any code can be valid (or) for the content to be rendered.
+	 *
+	 * @param array $atts attributes
+	 * @param string $content content to render
+	 * @return string
+	 */
+	public static function coupon_is_not_valid( $atts, $content = null ) {
+		$output = '';
+		if ( !empty( $content ) ) {
+			$valid = !self::_is_valid( $atts );
+			if ( $valid ) {
+				remove_shortcode( 'coupon_is_not_valid' );
+				$content = do_shortcode( $content );
+				add_shortcode( 'coupon_is_not_valid', array( __CLASS__, 'coupon_is_not_valid' ) );
+				$output = $content;
+			}
+		}
+		return $output;
+	}
+
+	/**
+	 * Returns an array of (potential) coupon codes obtained
+	 * through the options or through the global that might have been
+	 * set in _is_valid.
+	 * 
+	 * @param array $options
+	 * @return array
+	 */
+	private static function get_codes( $options ) {
+		global $woocommerce_coupon_shortcodes_codes;
+		$codes = array();
+		$code = null;
+		if ( !empty( $options['code'] ) ) {
+			$code = $options['code'];
+		} else if ( !empty( $options['coupon'] ) ) {
+			$code = $options['coupon'];
+		}
+		if ( $code === null ) {
+			if ( !empty( $woocommerce_coupon_shortcodes_codes ) ) {
+				$codes = $woocommerce_coupon_shortcodes_codes;
+			} else {
+				return '';
+			}
+		}
+		if ( empty( $codes ) ) {
+			$codes = array_map( 'trim', explode( ',', $code ) );
+		}
+		return $codes;
+	}
+
+	/**
+	 * Renders the code(s) of coupon(s).
+	 *
+	 * @param array $atts
+	 * @param string $content not used
+	 * @return string
+	 */
+	public static function coupon_code( $atts, $content = null ) {
+
+		$output = '';
+		$options = shortcode_atts(
+			array(
+				'coupon'    => null,
+				'code'      => null,
+				'separator' => ' '
+			),
+			$atts
+		);
+
+		$codes = self::get_codes( $options );
+		foreach ( $codes as $code ) {
+			$coupon = new WC_Coupon( $code );
+			if ( $coupon->id ) {
+				$output .= sprintf( '<span class="coupon code %s">', stripslashes( wp_strip_all_tags( $coupon->code ) ) );
+				$output .= stripslashes( wp_strip_all_tags( $coupon->code ) );
+				$output .= '</span>';
+				$output .= stripslashes( wp_filter_kses( $options['separator'] ) );
+			}
+		}
+		return $output;
+	}
+
+	/**
+	 * Renders the description(s) of coupon(s).
+	 * 
+	 * @param array $atts
+	 * @param string $content not used
+	 * @return string
+	 */
+	public static function coupon_description( $atts, $content = null ) {
+		$output = '';
+		$options = shortcode_atts(
+			array(
+				'coupon'    => null,
+				'code'      => null,
+				'separator' => ' '
+			),
+			$atts
+		);
+
+		$codes = self::get_codes( $options );
+		foreach ( $codes as $code ) {
+			$coupon = new WC_Coupon( $code );
+			if ( $coupon->id ) {
+				if ( $post = get_post( $coupon->id ) ) {
+					if ( !empty( $post->post_excerpt ) ) {
+						$output .= sprintf( '<span class="coupon description %s">', stripslashes( wp_strip_all_tags( $coupon->code ) ) );
+						$output .= stripslashes( wp_filter_kses( $post->post_excerpt ) );
+						$output .= '</span>';
+						$output .= stripslashes( wp_filter_kses( $options['separator'] ) );
+					}
+				}
+			}
+		}
+		return $output;
+	}
+
+	/**
+	 * Renders information about the discount for coupon(s).
+	 *
+	 * @param array $atts
+	 * @param string $content not used
+	 * @return string
+	 */
+	public static function coupon_discount( $atts, $content = null ) {
+		$output = '';
+		$options = shortcode_atts(
+			array(
+				'coupon'    => null,
+				'code'      => null,
+				'separator' => ' ',
+				'renderer'  => 'auto'
+			),
+			$atts
+		);
+
+		$codes = self::get_codes( $options );
+		foreach ( $codes as $code ) {
+			$coupon = new WC_Coupon( $code );
+			if ( $coupon->id ) {
+				$output .= sprintf( '<span class="coupon discount %s">', stripslashes( wp_strip_all_tags( $coupon->code ) ) );
+				
+				$renderer = null;
+				if ( $options['renderer'] == 'auto' ) {
+
+					// WooCommerce_Coupons_Countdown_Shortcodes
+					// does not differ
+
+					// WooCommerce_Groupons_Shortcodes
+					// does not differ
+
+					// WooCommerce_Volume_Discount_Coupons_Shortcodes
+					if ( class_exists( 'WooCommerce_Volume_Discount_Coupons_Shortcodes' ) ) {
+						$min = get_post_meta( $coupon->id, '_vd_min', true );
+						$max = get_post_meta( $coupon->id, '_vd_max', true );
+						if ( ( $min > 0 ) || ( $max > 0 ) ) {
+							$renderer = 'WooCommerce_Volume_Discount_Coupons_Shortcodes';
+						}
+					}
+
+				}
+
+				if ( $renderer === null ) {
+					$output .= self::get_discount_info( $coupon, $atts );
+				} else {
+					switch( $renderer ) {
+						case 'WooCommerce_Volume_Discount_Coupons_Shortcodes' :
+							$output .= WooCommerce_Volume_Discount_Coupons_Shortcodes::get_volume_discount_info( $coupon );
+							break;
+					}
+				}
+				$output .= '</span>';
+				$output .= stripslashes( wp_filter_kses( $options['separator'] ) );
+			}
+		}
+		return $output;
+	}
+
+	/**
+	 * Returns a description of the discount.
+	 *
+	 * @param WC_Coupon $coupon
+	 * @return string HTML describing the discount
+	 */
+	public static function get_discount_info( $coupon, $atts = array() ) {
+		$product_delimiter = isset( $atts['product_delimiter'] ) ? $atts['product_delimiter'] : ', ';
+		$category_delimiter = isset( $atts['category_delimiter'] ) ? $atts['category_delimiter'] : ', ';
+		$result = '';
+
+		$amount_suffix = get_woocommerce_currency_symbol();
+		switch( $coupon->type ) {
+			case 'percent' :
+			case 'percent_product' :
+				$amount_suffix = '%';
+				break;
+		}
+
+		$products = array();
+		$categories = array();
+		switch ( $coupon->type ) {
+			case 'fixed_product' :
+			case 'percent_product' :
+				if ( sizeof( $coupon->product_ids ) > 0 ) {
+					foreach( $coupon->product_ids as $product_id ) {
+						$product = get_product( $product_id );
+						if ( $product ) {
+							$products[] = sprintf(
+								'<span class="product-link"><a href="%s">%s</a></span>',
+								esc_url( get_permalink( $product_id ) ),
+								$product->get_title()
+							);
+						}
+					}
+				}
+				if ( sizeof( $coupon->product_categories ) > 0 ) {
+					foreach( $coupon->product_categories as $term_id ) {
+						if ( $term = get_term_by( 'id', $term_id, 'product_cat' ) ) {
+							$categories[] = sprintf(
+								'<span class="product-link"><a href="%s">%s</a></span>',
+								get_term_link( $term->slug, 'product_cat' ),
+								esc_html( $term->name )
+							);
+						}
+					}
+				}
+				break;
+		}
+
+		switch ( $coupon->type ) {
+			case 'fixed_product' :
+			case 'percent_product' :
+				if ( sizeof( $coupon->product_ids ) > 0 ) {
+					if ( count( $products ) > 0 ) {
+						$result = sprintf( __( '%s%s Discount on %s', WOO_CODES_PLUGIN_DOMAIN ), $coupon->amount, $amount_suffix, implode( $product_delimiter, $products ) );
+					} else {
+						$result = sprintf( __( '%s%s Discount on selected products', WOO_CODES_PLUGIN_DOMAIN ), $coupon->amount, $amount_suffix );
+					}
+				} else if ( sizeof( $coupon->product_categories ) > 0 ) {
+					$result = sprintf( __( '%s%s Discount in %s', WOO_CODES_PLUGIN_DOMAIN ), $coupon->amount, $amount_suffix, implode( $category_delimiter, $categories ) );
+				} else if ( sizeof( $coupons->exclude_product_ids ) > 0 || sizeof( $coupon->exclude_product_categories ) > 0 ) {
+					$result = sprintf( __( '%s%s Discount on selected products', WOO_CODES_PLUGIN_DOMAIN ), $coupon->amount, $amount_suffix );
+				} else {
+					$result = sprintf( __( '%s%s Discount', WOO_CODES_PLUGIN_DOMAIN ), $coupon->amount, $amount_suffix );
+				}
+
+				break;
+			case 'fixed_cart' :
+			case 'percent' :
+				$result = sprintf( __( '%s%s Discount', WOO_CODES_PLUGIN_DOMAIN ), $coupon->amount, $amount_suffix );
+				break;
+		}
+
+		return apply_filters( 'woocommerce_coupon_shortcodes_info', $result, $coupon );
+	}
+
+}
+WooCommerce_Coupon_Shortcodes_Views::init();
---
+++ readme.txt
@@ -0,0 +1,91 @@
+=== WooCommerce Coupon Shortcodes ===
+Contributors: itthinx
+Donate link: http://www.itthinx.com/plugins/woocommerce-coupon-shortcodes
+Tags: conditional, coupon, coupons, discount, discounts, display, info, information, marketing, promotion, shortcode, shortcodes, woocommerce
+Requires at least: 3.3
+Tested up to: 3.6.1
+Stable tag: 1.0.0
+License: GPLv3
+
+Show coupon discount info using shortcodes. Allows to render coupon information and content conditionally, based on the validity of coupons.
+
+== Description ==
+
+This extension for [WooCommerce](http://wordpress.org/extend/plugins/woocommerce) allows you to render coupon information and show content based on the validity of coupons.
+
+Customers can be motivated to proceed with their purchase, offering them to use specific coupons
+when the contents in the cart qualify for it, or by offering them to purchase additional items
+so they can use a coupon.
+
+Extended coupon discount info for volume discounts is shown automatically, if the [WooCommerce Volume Discount Coupons](http://www.itthinx.com/plugins/woocommerce-volume-discount-coupons) is installed.
+
+= Conditional Shortcodes =
+
+It provides the conditional shortcodes
+
+`[coupon_is_valid]` and
+`[coupon_is_not_valid]`
+
+that allow to enclose content which is shown if the coupon is (or is not) valid.
+
+= Coupon Info Shortcodes =
+
+It also provides shortcodes that allow to render the coupon code, its description and an automatic description of the discount:
+
+`[coupon_code]` (this one makes sense mostly when used inside one of the conditional shortcodes).
+`[coupon_description]`
+`[coupon_discount]`
+
+= Examples =
+
+Showing a coupon when the cart contents qualify for a coupon to be applied: 
+
+`[coupon_is_valid code="superdiscount"]
+You qualify for a discount!
+Use the coupon code [coupon_code] to take advantage of this great discount : [coupon_discount]
+[/coupon_is_valid]`
+
+Showing a coupon that is not valid for the current cart and motivating to add items:
+
+`[coupon_is_not_valid code="25off"]
+If you purchase 5 Widgets, you can use the coupon [coupon_code] to get 25% off your purchase!
+[/coupon_is_not_valid]`
+
+= Documentation and Support =
+
+Full usage instructions and help is provided on these pages:
+
+- [Documentation](http://www.itthinx.com/documentation/woocommerce-coupon-shortcodes/")
+- [WooCommerce Coupon Shortcodes plugin page and Support](http://www.itthinx.com/plugins/woocommerce-coupon-shortcodes/)
+
+
+== Installation ==
+
+1. Upload or extract the `woocommerce-coupon-shortcodes` folder to your site's `/wp-content/plugins/` directory. You can also use the *Add new* option found in the *Plugins* menu in WordPress.  
+2. Enable the plugin from the *Plugins* menu in WordPress.
+
+== Frequently Asked Questions ==
+
+= Where is the documentation? =
+
+[Documentation](http://www.itthinx.com/documentation/woocommerce-coupon-shortcodes/")
+
+= I have a question, where do I ask? =
+
+You can leave a comment at the [WooCommerce Coupon Shortcodes plugin page](http://www.itthinx.com/plugins/woocommerce-coupon-shortcodes/).
+
+
+== Screenshots ==
+
+See the plugin page [Groups](http://www.itthinx.com/plugins/woocommerce-discount-coupons/)
+
+
+== Changelog ==
+
+= 1.0.0 =
+* Initial release.
+
+== Upgrade Notice ==
+
+= 1.0.0 =
+* Initial release.
